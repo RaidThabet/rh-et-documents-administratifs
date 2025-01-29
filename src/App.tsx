0a9f67f8864105934989f54758dd4d3c61b3803b
Raid Thabet <thabet.raid123@gmail.com>
2025-01-29 19:06:25 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {HeroUIProvider} from "@heroui/system";
+
+function App() {
+    return (
+        <HeroUIProvider>
+            <div>
+
+            </div>
+        </HeroUIProvider>
+
+    );
+}
+
+export default App;
