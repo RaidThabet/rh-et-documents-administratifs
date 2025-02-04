75d160e980cd0a649cc8fe80f2569c5935b7f57a
Raid Thabet <thabet.raid123@gmail.com>
2025-02-04 10:14:49 +0100

    Improved Sidebar animation, and removed categories from Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
 import {Avatar} from "@heroui/avatar";
-import {Categories, SidebarCategory} from "../types/sidebarCategory";
 import {NavLink} from "react-router";
 import {v4 as uuidv4} from "uuid";
 import SidebarButton from "./SidebarButton.tsx";
@@ -10,35 +9,15 @@ import {useState} from "react";
 import {TbLayoutSidebar} from "react-icons/tb";
 import { motion } from "framer-motion";
 
-const categories: Categories = [
-    {
-        name: "Administration Générale",
-        buttons: [
-            {icon: <FaUserAlt size={15} />, label: "Gestion des utilisateurs", href: "users-management"},
-            {icon: <IoSettings size={15} />, label: "Paramètres système", href: "system-settings"},
-        ]
-    },
-    {
-        name: "Gestion du Personnel",
-        buttons: [
-            {icon: <FaDatabase size={15} />, label: "Employés et enseignants", href: "employees-profs-management"},
-            {icon: <IoDocumentText size={15} />, label: "Documents administratifs", href: "documents"},
-        ]
-    },
-    {
-        name: "Gestion du Temps & des Présences",
-        buttons: [
-            {icon: <FaCalendarTimes size={15} />, label: "Absences et congés", href: "absences-and-leaves"},
-            {icon: <IoIosTime size={15} />, label: "Emploi du temps", href: "time-table"},
-        ]
-    },
-    {
-        name: "Organisation & Suivi",
-        buttons: [
-            {icon: <FaTasks size={15} />, label: "Tâches et responsabilités", href: "tasks-and-responsibilities"},
-            {icon: <IoStatsChart size={15} />, label: "Statistiques et rapports", href: "stats-and-reports"},
-        ]
-    }
+const buttons= [
+    {icon: <FaUserAlt size={15} />, label: "Gestion des utilisateurs", href: "users-management"},
+    {icon: <IoSettings size={15} />, label: "Paramètres système", href: "system-settings"},
+    {icon: <FaDatabase size={15} />, label: "Employés et enseignants", href: "employees-profs-management"},
+    {icon: <IoDocumentText size={15} />, label: "Documents administratifs", href: "documents"},
+    {icon: <FaCalendarTimes size={15} />, label: "Absences et congés", href: "absences-and-leaves"},
+    {icon: <IoIosTime size={15} />, label: "Emploi du temps", href: "time-table"},
+    {icon: <FaTasks size={15} />, label: "Tâches et responsabilités", href: "tasks-and-responsibilities"},
+    {icon: <IoStatsChart size={15} />, label: "Statistiques et rapports", href: "stats-and-reports"},
 ]
 
 function Sidebar() {
@@ -48,43 +27,34 @@ function Sidebar() {
         setIsCollapsed(prev => !prev);
     }
 
-    const renderCategory = (category: SidebarCategory) => {
-        const buttons = (
-            <div className={"flex flex-col justify-start items-center gap-3 w-full"}>
+    const renderButtons = () => {
+        return (
+            <div className={"flex flex-col justify-center items-center gap-3 w-full"}>
                 {
-                    category.buttons.map(({icon, label, href}) => (
-                        <div>
-                            <NavLink key={uuidv4()} to={href} className={"flex justify-start items-center w-full"}
+                    buttons.map(({icon, label, href}) => (
+                            <NavLink key={uuidv4()} to={href} className={"flex justify-center items-center w-full"}
                             >
                                 {
                                     ({isActive}) => (
-                                        <SidebarButton isCollapsed={isCollapsed} icon={icon} label={label} isActive={isActive} />
+                                        <SidebarButton isCollapsed={isCollapsed} icon={icon} label={label}
+                                                       isActive={isActive}/>
                                     )
                                 }
                             </NavLink>
-                        </div>
-
                     ))}
             </div>
         )
-
-        return (
-            <div key={uuidv4()} className={"flex flex-col justify-center items-start w-full"}>
-                {buttons}
-            </div>
-        );
-
     }
 
     return (
         <motion.nav
             layout
-            transition={{type: "spring", duration: 1}}
+            transition={{duration: 0.3, ease: "easeInOut"}}
             className={"z-30 absolute left-0 px-4 flex flex-col justify-start items-start gap-3 bg-neutral-200 h-full"}> {/*Sidebar main container*/}
-            <div className={"flex flex-row justify-start items-center py-3 gap-3"}> {/*Avatar container*/}
+            <div className={"w-full flex flex-row justify-start items-center py-3 gap-3"}> {/*Avatar container*/}
                 <motion.button layout className={"absolute bottom-1 left-1"} onClick={handleCollapse}><TbLayoutSidebar size={22} /></motion.button>
                 <motion.div layout>
-                    <Avatar size={"lg"} src={"/public/images/logo_isimm.png"}/>
+                    <Avatar size={"md"} src={"/public/images/logo_isimm.png"}/>
                 </motion.div>
                 {!isCollapsed && (
                     <motion.p
@@ -98,11 +68,7 @@ function Sidebar() {
                     </motion.p>
                 )}
             </div>
-            {
-                categories.map(category => (
-                    renderCategory(category)
-                ))
-            }
+            {renderButtons()}
         </motion.nav>
     );
 }
--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -11,13 +11,16 @@ type Props = {
 
 function SidebarButton({isCollapsed, icon, label, isActive}: Props) {
     return (
-        <motion.div
-            layout
-            className={clsx("flex flex-row justify-start items-center gap-3 py-2 px-3 text-sm font-semibold rounded-md h-10 flex-1 w-full", {
+        <motion.button
+            // layout
+            className={clsx("flex flex-row justify-start items-center gap-3 py-2 px-3 text-sm font-semibold rounded-md h-10 w-full", {
                 "font-bold bg-neutral-300": isActive,
                 "hover:bg-neutral-300 transition-background drop-shadow": !isActive,
-                "w-fit": isCollapsed
-            })}>
+                // "w-full": !isCollapsed,
+                // "w-fit": isCollapsed
+
+            })}
+        >
             <motion.div layout>
                 {icon}
             </motion.div>
@@ -29,7 +32,7 @@ function SidebarButton({isCollapsed, icon, label, isActive}: Props) {
                     transition={{delay: 0.125}}
                 >{label}</motion.span>
             )}
-        </motion.div>
+        </motion.button>
     );
 }
 
