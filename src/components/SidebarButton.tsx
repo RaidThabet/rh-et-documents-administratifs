14fb138456e54f928d762aaf78c237fdbe1d9ce8
Raid Thabet <thabet.raid123@gmail.com>
2025-02-05 10:59:42 +0100

    Updated tooltip

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,6 +1,7 @@
 import {clsx} from "clsx";
-import {ReactNode, useState} from "react";
+import {ReactNode} from "react";
 import {motion} from "framer-motion";
+import {Tooltip} from "@heroui/tooltip";
 
 type Props = {
     icon?: ReactNode;
@@ -10,54 +11,32 @@ type Props = {
 }
 
 function SidebarButton({isCollapsed, icon, label, isActive}: Props) {
-    const [isHovered, setIsHovered] = useState<boolean>(false);
-
-    let hoverTimeout: number | null | undefined = null;
-
-    const handleHoverStart = () => {
-        hoverTimeout = setTimeout(() => {
-            setIsHovered(true);
-        }, 800);
-    }
-
-    const handleHoverEnd = () => {
-        if (hoverTimeout) {
-            clearTimeout(hoverTimeout)
-        }
-        setIsHovered(false);
-    }
-
     return (
-        <motion.button
-            onClick={handleHoverEnd}
-            onHoverStart={handleHoverStart}
-            onHoverEnd={handleHoverEnd}
-            // layout
-            className={clsx("relative flex flex-row justify-start items-center gap-3 py-2 px-3 text-sm font-semibold rounded-md h-10 w-full", {
-                "font-bold text-[#0D47A1] bg-[#cce6f4]": isActive,
-                "hover:bg-[#cce6f4] hover:text-[#0D47A1] transition-background drop-shadow": !isActive,
-                // "w-full": !isCollapsed,
-                // "w-fit": isCollapsed
+        <Tooltip isDisabled={!isCollapsed} color={"primary"} content={label} placement={"right"} delay={600}>
+            <motion.button
+                // layout
+                className={clsx("relative flex flex-row justify-start items-center gap-3 py-2 px-3 text-sm font-semibold rounded-md h-10 w-full", {
+                    "font-bold text-[#0D47A1] bg-[#cce6f4]": isActive,
+                    "hover:bg-[#cce6f4] hover:text-[#0D47A1] transition-background drop-shadow": !isActive,
+                    // "w-full": !isCollapsed,
+                    // "w-fit": isCollapsed
 
-            })}
-        >
-            <motion.div layout>
-                {icon}
-            </motion.div>
-            {!isCollapsed && (
-                <motion.span
-                    layout
-                    initial={{opacity: 0, y: 12}}
-                    animate={{opacity: 1, y: 0}}
-                    transition={{delay: 0.125}}
-                >{label}</motion.span>
-            )}
-            <div className={clsx("text-black opacity-0 bg-neutral-200 p-1 absolute left-full top-1/2 -translate-y-1/2 whitespace-nowrap ml-2 px-2 py-1 pointer-events-none", {
-                "opacity-100": isHovered && isCollapsed
-            })}>
-                {label}
-            </div>
-        </motion.button>
+                })}
+            >
+                <motion.div layout>
+                    {icon}
+                </motion.div>
+                {!isCollapsed && (
+                    <motion.span
+                        layout
+                        initial={{opacity: 0, y: 12}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{delay: 0.125}}
+                    >{label}</motion.span>
+                )}
+            </motion.button>
+        </Tooltip>
+
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/components/(accordion|avatar|breadcrumbs|button|card|chip|divider|drawer|dropdown|input|modal|navbar|pagination|select|table|user|ripple|spinner|menu|popover|form|listbox|scroll-shadow|checkbox|spacer).js"
+    "./node_modules/@heroui/theme/dist/components/(accordion|avatar|breadcrumbs|button|card|chip|divider|drawer|dropdown|input|modal|navbar|pagination|select|table|popover|user|ripple|spinner|menu|form|listbox|scroll-shadow|checkbox|spacer).js"
   ],
   theme: {
     extend: {},
