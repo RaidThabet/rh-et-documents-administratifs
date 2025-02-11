e0805b34fd04d07bcf0b7d4dad932b7cb9b573c4
NassimKraiem <kakik2003@gmail.com>
2025-02-11 14:37:20 +0100

    added reset password backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,9 +5,17 @@ const rbacMiddleware = require('../middleware/rbacMiddleware');
 const authController = require('../controllers/authController');
 
 router.post('/login', rbacMiddleware.checkPermission('login'), authController.loginUser);
+
 router.post('/register', rbacMiddleware.checkPermission('register'), authController.registerUser);
+
+router.post('/forgot', rbacMiddleware.checkPermission('reset_password'), authController.forgotPassword);
+router.get('/forgot/check', rbacMiddleware.checkPermission('reset_password'), authController.checkResetCredentials);
+router.post('/reset', rbacMiddleware.checkPermission('"reset_password"'), authController.resetPassword);
+
 router.get('/logout', rbacMiddleware.checkPermission('logout'), authController.logoutUser);
-router.get('/status', rbacMiddleware.checkPermission('logout'), authController.loggedIn);
+
+router.get('/status', rbacMiddleware.checkPermission('login'), authController.loggedIn);
+
 router.put('/:id', rbacMiddleware.checkPermission('reset_password'), authController.resetPassword);
 router.delete('/:id', rbacMiddleware.checkPermission('delete_auth'), authController.deleteUser);
 
