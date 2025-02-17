c218cbe39c1beb6df2afb9c60b3b512bd78563bf
Raid Thabet <thabet.raid123@gmail.com>
2025-02-17 14:05:34 +0100

    Integrated tanstack query into the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,12 @@ import {Navigate, Outlet} from "react-router";
 import Sidebar from "./components/Sidebar.tsx";
 import Header from "./components/Header.tsx";
 import {useAuth} from "./hooks/useAuth.ts";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 function App() {
-    const { isLoggedIn, loading } = useAuth();
+    const {isLoggedIn, loading} = useAuth();
 
     if (loading) {
         return <div className={"text-4xl flex justify-center items-center h-[100vh]"}>Chargement...</div>; // Or your loading component
@@ -13,19 +16,22 @@ function App() {
 
     if (!isLoggedIn) {
         console.log("not authenticated");
-        return <Navigate to="/login" />;
+        return <Navigate to="/login"/>;
     }
 
     return (
-        <HeroUIProvider>
-            <div className={"flex flex-row justify-center items-center h-[100vh]"}>
-                <Sidebar/>
-                <div className={"overflow-scroll flex flex-col justify-start items-start flex-1 h-full"}>
-                    <Header />
-                    <Outlet/>
+        <QueryClientProvider client={queryClient}>
+            <HeroUIProvider>
+                <div className={"flex flex-row justify-center items-center h-[100vh]"}>
+                    <Sidebar/>
+                    <div className={"overflow-scroll flex flex-col justify-start items-start flex-1 h-full"}>
+                        <Header/>
+                        <Outlet/>
+                    </div>
                 </div>
-            </div>
-        </HeroUIProvider>
+            </HeroUIProvider>
+        </QueryClientProvider>
+
     );
 }
 
